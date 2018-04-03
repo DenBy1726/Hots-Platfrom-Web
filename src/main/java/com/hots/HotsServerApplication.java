ac0e77f28b51174da6fdf923abb2fe5afaf1cf35
DenBy1726 <=>
2018-04-03 22:12:55 +0300

    Initial commit

---
+++ src/main/java/com/hots/HotsServerApplication.java
@@ -0,0 +1,13 @@
+package com.hots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HotsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotsServerApplication.class, args);
+	}
+}
